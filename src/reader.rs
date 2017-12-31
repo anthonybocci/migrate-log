7221adfe790f97e3074d2cbef6b1edaaeeb3f15a
Anthony Bocci <boccianthony@yahoo.fr>
2017-12-31 23:39:47 +0100

    Reformat the code and move the reader function into reader.rs
    read_lines returns a Vec<log::Log>, not a Vec<String>

---
+++ src/reader.rs
@@ -0,0 +1,28 @@
+use log;
+
+use std::fs::File;
+use std::io::prelude::*;
+
+/// Reads the content of a file and sets it `content`.
+/// Panics if the file can't be open.
+pub fn read_content(filename: &str, content: &mut String) {
+    let log_file_name = filename;
+    let mut log_file = File::open(log_file_name).unwrap();
+    //Reads the log file
+    log_file.read_to_string(content).unwrap();
+}
+
+/// Reads all lines in a file and set a Vector of Log.
+///
+/// # Arguments
+///
+/// * filename  - The name of the file to read
+/// * lines     - The Vector that will contain the Logs.
+pub fn read_lines(filename: &str, lines: &mut Vec<log::Log>) {
+    let mut content = String::new();
+    read_content(filename, &mut content);
+
+    for line in content.lines() {
+        lines.push(log::Log::from_line(line))
+    }
+}
